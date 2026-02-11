15938bbf32cb644e9c5ca59373500e2ff4a71dfa
Priyanga Ganapathi <priyanga.ganapathi@zoomrx.com>
2026-02-11 19:30:26 +0530

    CI/CD related changes

---
+++ deploy/jenkins/dev/entity-extraction-agent-indication-extraction-worker.groovy
@@ -0,0 +1,12 @@
+@Library('congress-ci-cd@main') _
+
+ferma_congress_dev.call(
+  repo_url: "https://github.com/ZoomRx/entity-extraction-agent.git",
+  deployment_type: "backend",
+  application_name: "entity-extraction-agent-indication-extraction-worker",
+  chart_version: "v3.1.0",
+  mode_parameters: "build_deploy,deploy,rollback,restart",
+  dockerfile_dir: "deploy/entity-extraction-agent/",
+  chart_name: "application_chart",
+  image_name: "entity-extraction-agent"
+)
