ba8c0bfff9f94b3c97a724a869f20c0c67a495a4
Priyanga Ganapathi <priyanga.ganapathi@zoomrx.com>
2026-03-19 19:33:04 +0530

    Fixed issues in CI/CD configs

--- deploy/jenkins/prod/entity-extraction-agent-extraction-progress-worker.groovy
+++ deploy/jenkins/prod/entity-extraction-agent-extraction-progress-worker.groovy
@@ -3,7 +3,7 @@
 ferma_congress_prod.call(
   repo_url: "https://github.com/ZoomRx/entity-extraction-agent.git",
   deployment_type: "backend",
-  application_name: "entity-mapping-agent-extraction-progress-worker",
+  application_name: "entity-extraction-agent-extraction-progress-worker",
   chart_version: "v3.1.0",
   mode_parameters: "deploy,rollback,restart",
   dockerfile_dir: "deploy/entity-extraction-agent/",
